b6ebec8efa798a1dcf53e3e52fa8b14533a48a92
ayazali232006-blip <ayazali232006@gmail.com>
2026-01-05 19:38:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ st.title("I am Ayaz Ali")
 st.header("This Is My First Streamlit App")
 st.write("Hello")
 
-st.sidebar.title("Sidebar")
-
-
+st.sidebar.title("Mainu")
+st.sidebar.selectbox("Here Are All Data Structure", ["Data Frame", "Chart"])
 
 
